ef7ca96bd78f8709c640603b890f9e65f584877b
leonardobvieira22 <leonardobvieira22@github.com>
2025-09-20 03:57:23 -0300

    fix: Usar snprintf e buffer maior para formatDate

--- Servidor 8.60 Atualizado/Global/src/tools.cpp
+++ Servidor 8.60 Atualizado/Global/src/tools.cpp
@@ -320,9 +320,9 @@ std::string formatDate(time_t time)
 		return {};
 	}
 
-	char buffer[32];
-	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900, tms->tm_hour, tms->tm_min, tms->tm_sec);
-	if (res < 0) {
+	char buffer[80];
+	int res = snprintf(buffer, sizeof(buffer), "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900, tms->tm_hour, tms->tm_min, tms->tm_sec);
+	if (res < 0 || res >= static_cast<int>(sizeof(buffer))) {
 		return {};
 	}
 	return {buffer, static_cast<size_t>(res)};
